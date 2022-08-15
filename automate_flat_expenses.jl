30c590b6e33076d0336df3cb09963544b93331fc
herold1989 <jens_herold@gmx.net>
2022-08-15 16:19:48 +0200

    started playing with categorization

--- automate_flat_expenses.jl
+++ automate_flat_expenses.jl
@@ -4,25 +4,35 @@ wdir     = pwd()
 
 # add some basic packages
 using Dates
-using CSV
+using CSV, Tables
 using DataFrames
 using Plots
 
 
-# import raw sheet data
+# import raw sheet data -> Add your own file here
 
 filename = "wohnung_0822.csv"
 
+# headers, beginning of sheet might be different in your case
 raw_data  = DataFrame(CSV.File(wdir*"/"*filename, header = 9, skipto = 10))
 
 # clean data
 
-clean_data = DataFrame(Date = raw_data[!,1]);
+clean_data = DataFrame(Date = string.(raw_data[!,1]));
 
+# get necessary columns: Auftraggeber, Verwendungszweck und Betrag in my case
 header_clean = [names(raw_data)[4], names(raw_data)[5], names(raw_data)[8]]
 
 for (i,j) in enumerate(header_clean)
-
     clean_data[!,j].= raw_data[!,j];
-
 end
+
+# find regularly expenses
+
+# test = sort(clean_data, ([Symbol(names(raw_data)[4]),Symbol(names(raw_data)[5])]))
+
+
+# file_path = pwd()
+# file_name = "/test_data.csv";
+
+# CSV.write(file_path*file_name,  test, writeheader=false)
